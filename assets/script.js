cb1849799c29a138e1a3bb78552531b417460978
lily <lilyjulymm2009@gmail.com>
2024-06-07 12:59:24 +0630

    01-Create-Append

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+var listEl = document.createElement("ol");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+h1El.textContent = "Welcome to my page";
+kittenEl.textContent = "This is my kitten 🐱.";
+nameEl.textContent = "His name is Jax.";
+favoriteEl.textContent = "My favorite foods are:";
+li1.textContent = "Apples 🍎 ";
+li2.textContent = "Pizza 🍕 ";
+li3.textContent = "Dumplings 🥟 ";
+li4.textContent = "Cupcakes 🧁 ";
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+favoriteEl.appendChild(listEl);
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center; ");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+// Add styling to list element
+listEl.setAttribute("style", "background:#333333; padding:20px;");
+li1.setAttribute("style", " color:white; background: #444444; padding: 5px; margin-left: 35px;");
+li2.setAttribute("style", " color:white; background: #666666; padding: 5px; margin-left: 35px;");
+li3.setAttribute("style", " color:white; background: #111111; padding: 5px; margin-left: 35px;");
+li4.setAttribute("style", " color:white; background: #000000; padding: 5px; margin-left: 35px;");
+
